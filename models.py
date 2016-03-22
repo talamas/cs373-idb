60297523d016902a71c8041a7177647f6dc315b0
getzchris <getzchris@ymail.com>
2016-03-21 22:20:56 -0500

    Created SQLAlchemy Models and some initial files closes #9 closes #10

---
+++ models.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/test'
+app.config['SQLALCHEMY_ECHO'] = False
+db = SQLAlchemy(app)
+
+class car(db.Model):
+  '''
+  A model for a car, encapsulating the attributes we track for the car
+  and foreign key for the manufacturer.
+  '''
+
+  id = db.Column(db.Integer, primary_key=True)
+  make_id = db.Column(db.Integer, db.ForeignKey('manufacturer.id'))
+  make = db.relationship("Manufacturer")
+  model = db.Column(db.String(80), unique=True)
+  year = db.Column(db.Integer)
+  price = db.Column(db.Integer)
+  safety_rating = db.Column(db.Integer)
+
+  # def __repr__(self):
+  #     return '<Car Model %r>' % self.model
+
+class manufacturer(db.Model):
+  '''
+  A model for a manufacturer, encapsulating the attributes we track for
+  the manufacturer
+  '''
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(80), unique=True)
+  country = db.Column(db.String(80))
+  avg_saftey_rating = db.Column(db.Integer)
+  avg_price = db.Column(db.Integer)
+  num_models = db.Column(db.Integer)
+    # def __repr__(self):
+    #   return '<Manufacturer name %r>' % self.name
