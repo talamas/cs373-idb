25ab7157c4d30acd1823ffede89e1f66c8eb1580
getzchris <getzchris@ymail.com>
2016-03-23 18:12:31 -0500

    Moved files to static directory

--- app.py
+++ app.py
@@ -11,11 +11,29 @@ db = SQLAlchemy(app)
 @app.route('/')
 @app.route('/index')
 def index():
+  print("index path")
   return app.send_static_file('index.html')
+
+@app.route('/hello')
+def hello():
+  print("Hello")
+  return "not a page"
+
+@app.route('/img/<img_path>')
+def get_img(img_path):
+  print("image path")
+  print(img_path)
+  return app.send_static_file("/img/"+img_path)
+
+@app.route('/<src_path>')
+def get_src(src_path):
+  print("src path")
+  return app.send_static_file(src_path)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host = "0.0.0.0", port=5000, debug = True)
