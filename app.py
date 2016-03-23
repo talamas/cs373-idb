703fdc19092957e3552bc2d42d6cb217a3a7bd9e
getzchris <getzchris@ymail.com>
2016-03-23 14:50:04 -0500

    app working, serves only static index page

--- app.py
+++ app.py
@@ -2,24 +2,20 @@
 from flask import Flask, request, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/test'
 app.config['SQLALCHEMY_ECHO'] = False
 db = SQLAlchemy(app)
 
 @app.route('/')
-def hello_world():
-  return 'Hello World!'
-
 @app.route('/index')
 def index():
   return app.send_static_file('index.html')
 
-@app.route('/models/')
-def models():
-  print("Here\n")
-  return app.send_static_file('models.html')
+@app.errorhandler(404)
+def page_not_found(e):
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
   app.run()
