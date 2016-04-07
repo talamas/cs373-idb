8d5c049f8298a0cda30406307fd52d2ecc15098d
Lincoln Turner <lincoln.turner@utexas.edu>
2016-04-07 12:54:26 -0500

    Added API endpoints

--- app/app.py
+++ app/app.py
@@ -12,56 +12,57 @@ import json
 @app.route('/')
 @app.route('/index')
 def index():
-  #print("index path")
-  #####
   return send_file('index.html')
 
-@app.route('/testdata')
-def test_data():
-  # cars_json = {'cars' : []}
-  # cars = Car.query.all()
-  # for car in cars:
-  #   car_json = {}
-  #   car_json['id'] = car.id
-  #   car_json['make'] = car.make
-  #   car_json['model'] = car.model
-  #   car_json['year'] = car.year
-  #   car_json['price'] = car.price
-  #   car_json['horsepower'] = car.horsepower
-  #   cars_json['cars'].append(car_json)
-  asldkjasdffaskdf = 'asddfadf'
-  asdfs='asdfas'
-  car_file = open('cars_list.json')
-  cars_json = json.load(car_file)
+#HELPER FUNCTIONS
+def make_car_json(car):
+  car_json = {}
+  car_json['id'] = car.id
+  car_json['make'] = car.make
+  car_json['model'] = car.model
+  car_json['year'] = car.year
+  car_json['price'] = car.price
+  car_json['horsepower'] = car.horsepower
+  return car_json
+
+def make_man_json(man):
+  man_json = {}
+  man_json['id'] = man.id
+  man_json['name'] = man.name
+  man_json['num_models'] = man.num_models
+  man_json['avg_price'] = man.avg_price
+  man_json['most_expensive'] = man.most_expensive
+  man_json['avg_horsepower'] = man.avg_horsepower
+  return man_json
+
+#API ENDPOINTS
+@app.route('/get_cars')
+def get_cars():
+  cars_json = {'cars' : []}
+  cars = Car.query.all()
+  for car in cars:
+    cars_json['cars'].append(make_car_json(car))
   return jsonify(cars_json)
 
-# @app.route('/hello')
-# def hello():
-#   print("Hello")
-#   return "not a page"
+@app.route('/get_car/<int:car_id>')
+def get_car(car_id):
+  car = Car.query.filter(Car.id.like(car_id)).all()
+  return jsonify(make_car_json(car))
 
-# @app.route('/img/<img_path>')
-# def get_img(img_path):
-#   img_path = "static/img/" + img_path
-#   print("image path")
-#   print(img_path)
-#   fn, ext = os.path.splitext(img_path)
-#   return send_file(img_path, mimetype='image/'+ext)
+@app.route('/get_manufacturers')
+def get_manufacturers():
+  mans_json = {'mans' : []}
+  mans = Manufacturer.query.all()
+  for man in mans:
+    mans_json['mans'].append(make_man_json(man))
+  return jsonify(mans_json)
 
-# @app.route('/<src_path>')
-# def get_src(src_path):
-#   print("src path")
-#   return app.send_static_file(src_path)
+@app.route('/get_car/<int:manufacturer_id>')
+def get_car(manufacturer_id):
+  man = Manufacturer.query.filter(Manufacturer.id.like(manufacturer_id)).all()
+  return jsonify(make_man_json(man))
 
-
-#DATABASE
-class Car(db.Model):
-  __tablename__ = 'cars'
-  id = db.Column(db.Integer, primary_key= True)
-  name = db.Column(db.String(256), nullable=False)
->>>>>>> Stashed changes
-
-
+#DATABASE FUNCTIONS
 @manager.command
 def create_db():
   db.drop_all()
@@ -70,18 +71,6 @@ def create_db():
 @manager.command
 def drop_db():
   db.drop_all()
-
-#API
-# class CarQuery(Resource):
-#   def post(self):
-#     return {'status':'success'}
-
-# api.add_resource(CarQuery, '/CarQuery')
-
-
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
   app.run(host = "0.0.0.0", port=5000, debug = True)
