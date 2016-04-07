cb8cafd380af886357a4ba4ba50dad67fcd252e7
Adolfo <muller.adolfo@gmail.com>
2016-04-06 21:02:34 -0500

    added app.js and angular-route.min.js

---
+++ app/static/js/app.js
@@ -0,0 +1,27 @@
+/// <reference path="angular.min.js" />
+
+var lax = angular.module("sweatRidesApp", ["ngRoute"])
+	.config(function($routeProvider) {
+	    $routeProvider.
+	            when('/mans', {
+	                templateUrl: '../partials/manufacturersTable.html',
+	                controller: 'manTableCtrl'
+	            }).
+	            when('/cars', {
+	                templateUrl: '../partials/carsTable.html',
+	                controller: 'carsTableCtrl'
+	            }).
+	            when('/about', {
+	                templateUrl: '../partials/about.html',
+	                controller: 'aboutCtrl'
+	            })
+	})
+	.controller('manTableCtrl', function($scope) {
+	$scope.message = 'MANUFACTURERS';
+	})
+	.controller('carsTableCtrl', function($scope) {
+	$scope.message = 'CARS';
+	})
+	.controller('aboutCtrl', function($scope) {
+	$scope.message = 'ABOUT';
+	})
