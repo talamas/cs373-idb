3b817ce8e3ed70e23cedd4b78c27f114f27835ba
Adolfo <muller.adolfo@gmail.com>
2016-04-06 20:40:15 -0500

    test 1

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,28 +1,27 @@
 /// <reference path="angular.min.js" />
 
-var lax = angular.module("sweatRidesApp", ["ngRoute"]);
-
-lax.config(function($routeProvider) {
-    $routeProvider.
-            when('/mans', {
-                templateUrl: '../partials/manufacturersTable.html',
-                controller: 'manTableCtrl'
-            }).
-            when('/cars', {
-                templateUrl: '../partials/carsTable.html',
-                controller: 'carsTableCtrl'
-            }).
-            when('/about', {
-                templateUrl: '../partials/about.html',
-                controller: 'aboutCtrl'
-            })
-})
-.controller('manTableCtrl', function($scope) {
-$scope.message = 'This is Add new man info';
-})
-.controller('carsTableCtrl', function($scope) {
-$scope.message = 'This is car info';
-})
-.controller('aboutCtrl', function($scope) {
-$scope.message = 'This is about info';
-})
+var lax = angular.module("sweatRidesApp", ["ngRoute"])
+	.config(function($routeProvider) {
+	    $routeProvider.
+	            when('/mans', {
+	                templateUrl: '../partials/manufacturersTable.html',
+	                controller: 'manTableCtrl'
+	            }).
+	            when('/cars', {
+	                templateUrl: '../partials/carsTable.html',
+	                controller: 'carsTableCtrl'
+	            }).
+	            when('/about', {
+	                templateUrl: '../partials/about.html',
+	                controller: 'aboutCtrl'
+	            })
+	})
+	.controller('manTableCtrl', function($scope) {
+	$scope.message = 'This is Add new man info';
+	})
+	.controller('carsTableCtrl', function($scope) {
+	$scope.message = 'This is car info';
+	})
+	.controller('aboutCtrl', function($scope) {
+	$scope.message = 'This is about info';
+	})
