1424cb32ee3f3e9d961afaec3801645fbfe08617
Lincoln Turner <lincoln.turner@utexas.edu>
2016-04-07 20:20:33 -0500

    added API endpoint to run unit tests

--- app/app.py
+++ app/app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 from flask import *
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import *
 from flask.ext.script import Manager, Server
 from database import db, app, manager
 from setup_database import create_database
 from models import Car, Manufacturer
 #import requests
 import json
+import os
+import sys
+import subprocess
 
 #APP ROUTING
 @app.route('/')
@@ -38,6 +42,25 @@ def make_man_json(man):
 #API ENDPOINTS
 @app.route('/get_cars')
 def get_cars():
+  '''try:
+    car_json = open('static/json/cars_list.json', 'r')
+  except IOError as e:
+    return e.strerror
+  try:
+    car_data = json.load(car_json)
+  except:
+    return 'fail2'
+  return jsonify(car_data)
+  with open('cars_list.json') as car_json:
+    car_data = json.load(car_json)
+  return car_data
+  try:
+    cars = db.session.query(Car).all()
+  except:
+    e = sys.exc_info()[0]
+    return e
+  return 'didn\'t fail'
+  return len(cars)'''
   cars_json = {'cars' : []}
   cars = Car.query.all()
   for car in cars:
@@ -51,6 +74,7 @@ def get_car(car_id):
 
 @app.route('/get_manufacturers')
 def get_manufacturers():
+  #return os.path.join(os.path.dirname(__file__),'static/json/makes_list.json')
   mans_json = {'mans' : []}
   mans = Manufacturer.query.all()
   for man in mans:
@@ -61,6 +85,11 @@ def get_manufacturers():
 def get_manufacturer(manufacturer_id):
   man = Manufacturer.query.filter(Manufacturer.id.like(manufacturer_id)).all()
   return jsonify(make_man_json(man))
+
+@app.route('/unit_tests')
+def unit_tests():
+  output = subprocess.getoutput("python tests.py")
+  return json.dumps({'output': str(output)})
 
 #DATABASE FUNCTIONS
 @manager.command
