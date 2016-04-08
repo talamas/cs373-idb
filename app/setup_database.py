cdf17d525bce750dac4c69a4754114ce3a1d508b
Lincoln Turner <lincoln.turner@utexas.edu>
2016-04-07 20:06:37 -0500

    fixed some issues with models

--- app/setup_database.py
+++ app/setup_database.py
@@ -4,22 +4,30 @@ from database import db
 import json
 
 def create_cars(car_json):
-	for car in car_json['cars']:
-		car_model = Car(car['make'], car['model'], car['year'], car['price'], car['safety_rating'])
+	for car in car_json:
+		car_model = Car(car['make'], car['model'], car['year'], car['price'], car['horsepower'])
 		db.session.add(car_model)
 		db.session.commit()
 
 def create_manufacturers(manufacturer_json):
-	for man in manufacturer_json['manufacturers']:
-		manufacturer_model = Manufacturer(man['name'], man['safety'], man['price'], man['models'])
-		db.session.add(car_model)
+	for man in manufacturer_json:
+		manufacturer_model = Manufacturer(man['name'], man['num_models'], man['avg_price'], man['max_car_id'], man['avg_horsepower'])
+		db.session.add(manufacturer_model)
 		db.session.commit()
 
 def create_database():
 	db.create_all()
 
-	car_json = json.load(open('cars_list.json'))
+	'''car_json = json.load(open('cars_list.json'))
 	create_cars(car_json)
 
-	manufacturer_json = json.load(open('cars_list.json'))
-	create_manufacturers(manufacturer_json)
+	manufacturer_json = json.load(open('makes_list.json'))
+	create_manufacturers(manufacturer_json)'''
+
+	with open('cars_list.json') as car_json:
+		car_data = json.load(car_json)
+	create_cars(car_data)
+
+	with open('makes_list.json') as manufacurer_json:
+		manufacturer_data = json.load(manufacturer_json)
+	create_manufacturers(manufacturer_data)	
