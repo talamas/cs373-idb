4a9a215bf0dea454851d3d9e75dab6392d7a394b
David Talamas Cano <talamas@cs.utexas.edu>
2016-04-26 16:35:56 -0500

    data service for city/id

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -87,6 +87,22 @@ sweatRidesApp.service('dataService6', function($http) {
    };
 });
 
+sweatRidesApp.service('dataService7', function($http) {
+   delete $http.defaults.headers.common['X-Requested-With'];
+   this.getData = function(id) {
+       console.log("dataservice7");
+       // $http() returns a $promise that we can add handlers with .then()
+      return $http({
+         method: 'GET',
+         //url: 'http://sweetrides.me/get_manufacturers',
+	  url: '/get_city/'+id,
+         headers: {'Authorization': 'Token token=xxxxYYYYZzzz'}
+      });
+   };
+});
+
+
+
 /*
 sweatRidesApp.service('dataService7', function($http) {
    delete $http.defaults.headers.common['X-Requested-With'];
